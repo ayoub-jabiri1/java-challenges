dba1fdbe95a96c7902ad7704f8850c453552ee8a
Ayoub Jabiri <ayoubjabiri.coding@gmail.com>
2025-11-04 17:58:03 +0100

    Added the challenge 4

--- Main.java
+++ Main.java
@@ -26,27 +26,69 @@ public class Main {
 
 //        <-- Challenge 3 -->
 
-        Random random = new Random();
+//        Random random = new Random();
+//
+//        System.out.print("Enter a number: ");
+//        int num = Integer.parseInt(scanner.nextLine());
+//
+//        int randomNum = random.nextInt(10) + 1;
+//        boolean check = true;
+//
+//        while (check) {
+//
+//
+//            if (num == randomNum) {
+//                System.out.print("You guessed it right");
+//                check = false;
+//            } else {
+//                System.out.println("You guessed it wrong,");
+//                System.out.print("Try again: ");
+//                num = Integer.parseInt(scanner.nextLine());
+//            }
+//        }
 
-        System.out.print("Enter a number: ");
-        int num = Integer.parseInt(scanner.nextLine());
+//        <-- Challenge 4 -->
 
-        int randomNum = random.nextInt(10) + 1;
-        boolean check = true;
+        String choice;
+        int total = 0;
 
-        while (check) {
+        do {
+            System.out.println("1. Consulter le solde");
+            System.out.println("2. Dépôt");
+            System.out.println("3. Retrait");
+            System.out.println("4. Quitter");
 
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextLine();
 
-            if (num == randomNum) {
-                System.out.print("You guessed it right");
-                check = false;
-            } else {
-                System.out.println("You guessed it wrong,");
-                System.out.print("Try again: ");
-                num = Integer.parseInt(scanner.nextLine());
+            switch (choice) {
+                case "1":
+                    System.out.println("Your balance is: " + total);
+                    break;
+                case "2":
+                    System.out.print("Enter the amount: ");
+                    int amount = Integer.parseInt(scanner.nextLine());
+
+                    total += amount;
+                    System.out.println("Deposit successfully completed.");
+                    break;
+                case "3":
+                    System.out.print("Enter the withdraw amount: ");
+                    int withdraw = Integer.parseInt(scanner.nextLine());
+
+                    total -= withdraw;
+                    System.out.println("withdraw successfully completed.");
+                    break;
+                case "4":
+                    break;
+                default:
+                    System.out.println("The choice doesn't exists choose another!");
+
             }
-        }
 
+
+        } while (!choice.equals("4"));
+;
         scanner.close();
 
     }
